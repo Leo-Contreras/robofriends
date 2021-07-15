56f8f024ad39c4c9bbaa1cbe2291fe34d42c328f
Leo-Contreras <leonardoaguie@gmail.com>
2021-07-14 23:58:30 -0700

    finishing components

---
+++ src/SearchBox.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const SearchBox = () => {
+return (
+		<div className='tc'>
+		<input 
+		  className='pa3 ba b--green bg-lightest-blue'
+		  type ='search' 
+		  placeholder = 'Search Robots'
+		/>
+
+		</div>
+	);
+
+}
+
+
+
+export default SearchBox;
