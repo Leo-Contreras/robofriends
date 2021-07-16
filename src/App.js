436325a79aaf36c0f8ae767e1c0dee5132f487b6
Leo-Contreras <leonardoaguie@gmail.com>
2021-07-16 15:16:29 -0700

    Added Scrolling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import CardList from './CardList';
-import {robots} from './robots';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import './App.css'
 
 
@@ -11,26 +11,36 @@ class App extends React.Component {
 	constructor() {
 		super()
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchfield: ''
 	
 	}
-	}
+}
+
+componentDidMount(){
+	fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response => response.json())
+		.then(users => this.setState({ robots: users}));
+}
 
 	onSearchChange = (event) => {
 		this.setState({searchfield: event.target.value})
 		
 	}
 	render(){
-		const filteredRobots = this.state.robots.filter(robot =>
-		{
+		const filteredRobots = this.state.robots.filter(robot =>{
 			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
 		})
+		if(this.state.robots.length===0){
+			return <h1>Loading</h1>
+		}else 
 		return (
 			<div className = 'tc'>
 			<h1 className= 'f2' >RoboFriends</h1>
 			<SearchBox searchChange={this.onSearchChange}/>
+			<Scroll>
 			<CardList robots={filteredRobots} />
+			</Scroll>
 			</div>
 
 			);
